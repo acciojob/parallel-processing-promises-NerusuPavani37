c9f8fe040f2e486f86bb453c16b4750bfff7d311
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-27 20:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,30 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url){
+  return new Promise((resolve,reject)=>{
+    const img = new Image();
+    img.src = url;
+    img.onload=()=> resolve(img);
+    img.onerror=()=> reject(`Failed to load image: ${url}`);
+  })
+}
+
+function downloadImages(){
+  let promises = images.map(obj => downloadImage(obj.url));
+  Promise.all(promises)
+  .then(images =>{
+    images.forEach(image =>{
+      output.appendChild(image);
+    })
+  })
+  .catch((err)=>{
+    output.innerHTML += image;
+  })
+  .finally(()=>{
+     document.getElementById("loading").style.display = "none";
+  });
+}
+
+btn.addEventListener("click",downloadImages);
